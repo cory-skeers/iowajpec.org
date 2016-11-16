ae6ac39926709d3eef40ff245aab04e3c6cc44fb
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2016-11-16 14:41:50 -0600

    add jpec megamenu block template, delete tippie menu block template

---
+++ themes/jpec/templates/block/block--jpec-megamenu.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * Template for a block.
+ */
+?>
+<div id="block-jpec-megamenu-jpec-megamenu-main" class="collapse navbar-collapse block-jpec-megamenu">
+  <?php print $content ?>
+</div>
