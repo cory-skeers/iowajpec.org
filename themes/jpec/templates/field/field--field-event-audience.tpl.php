cee79c7519da69648abb380c34924843f4b85bfc
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2016-12-02 14:30:07 -0600

    add template for audiences tag (add commas between tags)

---
+++ themes/jpec/templates/field/field--field-event-audience.tpl.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * @file field--field-2x2-title.tpl.php
+ * Default template implementation to display the value of the 2x2 title.
+ *
+ * @see theme_field()
+ *
+ * Available variables:
+ * - $items: An array of field values. Use render() to output them.
+ * - $label: The item label.
+ * - $label_hidden: Whether the label display is set to 'hidden'.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions/. *
+ * Other variables:
+ * - $element['#object']: The entity to which the field is attached.
+ * - $element['#view_mode']: View mode, e.g. 'full', 'teaser'...
+ * - $element['#field_name']: The field name.
+ * - $element['#field_type']: The field type.
+ * - $element['#field_language']: The field language.
+ * - $element['#field_translatable']: Whether the field is translatable or not.
+ * - $element['#label_display']: Position of label display, inline, above, or
+ *   hidden.
+ * - $field_name_css: The css-compatible field name.
+ * - $field_type_css: The css-compatible field type.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess_field()
+ * @see theme_field()
+ *
+ * @ingroup themeable
+ */
+?>
+<div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <?php if (!$label_hidden) : ?>
+    <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
+  <?php endif; ?>
+  <div class="field-items"<?php print $content_attributes; ?>>
+    <?php foreach ($items as $delta => $item) : ?>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+        <?php 
+          print render($item);
+          // Add comma if not last item
+          if ($delta < (count($items) - 1)) {
+            print ','; 
+          }
+        ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
