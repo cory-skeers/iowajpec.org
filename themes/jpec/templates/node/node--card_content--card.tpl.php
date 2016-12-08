931e79ae00ae9eeb62389a0d658558159e00b491
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2016-12-08 15:19:08 -0600

    add template for content card "card" display

---
+++ themes/jpec/templates/node/node--card_content--card.tpl.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Radix theme implementation to display a node.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+<article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+  <div class="content"<?php print $content_attributes; ?>>
+
+    <?php print render($content['field_card_image']); ?>
+
+    <div class="card-description-wrapper">
+      <h3 class="node-title"><?php print render($content['field_card_title']); ?></h3>
+      <?php print render($content); ?>
+    </div>
+  </div>
+</article>
