a14f99d09d15f2bbd3a678cd26b1895fe1117cb6
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2016-12-16 18:49:57 -0600

    add columns to person teaser template

--- themes/jpec/templates/node/node--person--teaser.tpl.php
+++ themes/jpec/templates/node/node--person--teaser.tpl.php
@@ -12,29 +12,25 @@
  */
 ?>
 <article class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php print render($title_prefix); ?>
-    <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-  <?php print render($title_suffix); ?>
-
-    <?php
-      // Hide working title if a custom title is provided.
-
-      if (isset($content['field_person_custom_title'])) {
-       hide($content['field_person_title']);
-      }
-
-      hide($content['field_person_email']);
-      hide($content['field_phone']);
-
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
+  <div class="row">
+    <div class="col-sm-2 col-xs-4">
+      <?php print render($content['field_person_image']); ?>
+    </div>
+    <div class="col-sm-10 col-xs-8">
+      <?php print render($title_prefix); ?>
+        <h3<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
+      <?php print render($title_suffix); ?>
+      <?php
+        // We hide the comments and links now so that we can render them later.
+        hide($content['comments']);
+        hide($content['links']);
+        print render($content);
+      ?>
+    </div>
+  </div>
 
 
   <?php print render($content['links']); ?>
-
   <?php print render($content['comments']); ?>
 
 </article>
