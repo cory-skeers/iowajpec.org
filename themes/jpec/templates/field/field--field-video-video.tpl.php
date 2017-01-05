f95fd341a7f40df68e040587dfd37537f3f767d1
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2017-01-04 21:17:34 -0600

    add field template for field_video_video

---
+++ themes/jpec/templates/field/field--field-video-video.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file field--field-2x2-title.tpl.php
+ * Default template implementation to display the value of the 2x2 title.
+ *
+ * @see theme_field()
+ *
+ * Available variables:
+ * - $items: An array of field values. Use render() to output them.
+ * - $label: The item label.
+ * - $label_hidden: Whether the label display is set to 'hidden'.
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions/. *
+ * Other variables:
+ * - $element['#object']: The entity to which the field is attached.
+ * - $element['#view_mode']: View mode, e.g. 'full', 'teaser'...
+ * - $element['#field_name']: The field name.
+ * - $element['#field_type']: The field type.
+ * - $element['#field_language']: The field language.
+ * - $element['#field_translatable']: Whether the field is translatable or not.
+ * - $element['#label_display']: Position of label display, inline, above, or
+ *   hidden.
+ * - $field_name_css: The css-compatible field name.
+ * - $field_type_css: The css-compatible field type.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ *
+ * @see template_preprocess_field()
+ * @see theme_field()
+ *
+ * @ingroup themeable
+ */
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php foreach ($items as $delta => $item): ?>
+    <?php print render($item); ?>
+  <?php endforeach; ?>
+</div>
