ea66c588b303225b084245f9a1ee5cc877c203cd
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2017-08-31 15:08:22 -0500

    add custom formatting to the card events dates

--- themes/jpec/templates/node/node--event--card.tpl.php
+++ themes/jpec/templates/node/node--event--card.tpl.php
@@ -17,8 +17,32 @@
     
     <?php 
       hide($content['field_event_deadline_text']);
+      hide($content['field_event_date']);
 
-      print render($content['field_event_date']);
+      print '<div class="card-date">';
+
+      // create dt for start and end date
+      $start_date = DateTime::createFromFormat('Y-m-d H:i:s', $content['field_event_date']['#items'][0]['value']);
+      $end_date = DateTime::createFromFormat('Y-m-d H:i:s', $content['field_event_date']['#items'][0]['value2']);
+
+      // if event spans multiple days, print it in Month start - end, Year format
+      if ($start_date != $end_date) {
+
+        if (date_format($start_date, 'Y-m') == date_format($end_date, 'Y-m')) {
+          print date_format($start_date, 'F') . ' ' . date_format($start_date, 'd') . ' - ' . date_format($end_date, 'd') . ', ' . date_format($start_date, 'Y');
+        }
+        else {
+          // if start and end months are different, print the end month too
+          print date_format($start_date, 'F') . ' ' . date_format($start_date, 'd') . ' - ' . date_format($end_date, 'F') . ' ' . date_format($end_date, 'd') . ', ' . date_format($start_date, 'Y');
+        }
+      } 
+      else {
+        // start and end dates are the same, so printing normal date
+        print render($content['field_event_date']);
+      }
+
+      print '</div>';
+
       if (!empty($content['field_event_time'])) {
         print render($content['field_event_time']) . ' | ';
       } 
