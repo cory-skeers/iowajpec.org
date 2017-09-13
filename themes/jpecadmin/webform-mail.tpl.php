b8944c1a27f913b31852b22c637dc2eec806d2e7
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2017-09-13 09:24:33 -0500

    remove fieldsets and markups from the webform email template

--- themes/jpecadmin/webform-mail.tpl.php
+++ themes/jpecadmin/webform-mail.tpl.php
@@ -27,37 +27,55 @@
 <?php 
   $components = $node->webform['components'];
   foreach ($components as $key => $component) {
-    print '<tr><td style="background:#F2F2F2;padding: 5px;">';
-    print '<p style="font-size:14px;line-height:18px;font-family:Calibri,Arial,sans-serif;"><strong>' . $component['name'] . '</strong></p>';
+
+    $types_to_ignore = array('fieldset','markup');
+    if (!in_array($component['type'], $types_to_ignore)) {
     
-    if (!empty($submission->data[$key])) {
+      print '<tr><td style="background:#F2F2F2;padding: 5px;">';
+      print '<p style="font-size:14px;line-height:18px;font-family:Calibri,Arial,sans-serif;"><strong>' . $component['name'] . '</strong></p>';
+      
+      if (!empty($submission->data[$key])) {
+        
+        $submission_value = '';
 
-      $submission_value = '';
+        if ($component['type'] == 'select') {
 
-      if ($component['type'] == 'select') {
-        $options = array();
-        $items = explode("\n", $component['extra']['items']);
-        foreach ($items as $item) {
-          $item_parts = explode('|', $item);
-          if (isset($item_parts[0]) && isset($item_parts[1])) $options[$item_parts[0]] = $item_parts[1];
+          $options = array();
+          $items = explode("\n", $component['extra']['items']);
+          foreach ($items as $item) {
+
+            $item = trim($item);
+            $item_parts = explode('|', $item);
+
+            if (isset($item_parts[0]) && isset($item_parts[1])) {
+              $options[$item_parts[0]] = $item_parts[1]; 
+            }
+            else if (isset($item_parts[0])) {
+              $options[$item_parts[0]] = trim($item_parts[0]);
+            }
+          }
+
+          foreach ($submission->data[$key] as $j => $value) {
+            if (isset($options[$submission->data[$key][$j]])) {
+              $submission_value .= $options[$submission->data[$key][$j]] . '</br>';
+            }
+          }
+          
         }
-        if (isset($options[$submission->data[$key][0]])) {
-          $submission_value = $options[$submission->data[$key][0]];
+        else {
+          foreach ($submission->data[$key] as $k => $sub) {
+            $submission_value .= $sub;
+          }
         }
+      
+
+
+        print '</td></tr><tr><td style="padding:5px;padding-left:20px;">';
+        print '<p style="font-size:14px;line-height:18px;font-family:Calibri,Arial,sans-serif;">' . $submission_value . '</p>';
+        print '</td></tr>';
       }
-
-      else {
-        $submission_value = $submission->data[$key][0];
-      }
-    
-
-
-      print '</td></tr><tr><td style="padding:5px;padding-left:20px;">';
-      print '<p style="font-size:14px;line-height:18px;font-family:Calibri,Arial,sans-serif;">' . $submission_value . '</p>';
-      print '</td></tr>';
-    
+    }
   }
-}
 ?>
 
 </table>
