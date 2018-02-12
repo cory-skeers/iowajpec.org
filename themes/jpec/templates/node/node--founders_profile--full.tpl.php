40c6e9e75e7db55a38a7e4efec69d85867377b47
Alina Shybayeva <alina-shybayeva@uiowa.edu>
2018-02-12 15:27:56 -0600

    display the instagram link in the contact section of FC ct

--- themes/jpec/templates/node/node--founders_profile--full.tpl.php
+++ themes/jpec/templates/node/node--founders_profile--full.tpl.php
@@ -16,11 +16,12 @@ if (empty($content['field_founders_profile_location'])) {
 };
 
 $contact = 1;
-if (empty($content['field_founders_profile_phone']) && 
+if (empty($content['field_founders_profile_phone']) &&
     empty($content['field_founders_profile_email']) &&
     empty($content['field_founders_profile_url']) &&
     empty($content['field_founders_profile_facebook']) &&
-    empty($content['field_founders_profile_twitter'])) {
+    empty($content['field_founders_profile_twitter']) &&
+    empty($content['field_founders_profile_instagram'])) {
   $contact = 0;
 };
 ?>
@@ -33,49 +34,51 @@ if (empty($content['field_founders_profile_phone']) &&
       hide($content['field_founders_profile_url']);
       hide($content['field_founders_profile_facebook']);
       hide($content['field_founders_profile_twitter']);
+      hide($content['field_founders_profile_instagram']);
     ?>
 
 <article class="<?php print $classes; ?> clearfix container"<?php print $attributes; ?>>
       <div class="row">
-        
+
         <?php if (!empty($content['field_card_image']) && $content['field_card_image'] != ''):?>
           <div class="col-md-6">
             <?php print render($content['field_card_image']); ?>
           </div>
         <?php endif; ?>
-        
+
         <div class="col-md-6">
           <h2><?php print render($title); ?></h2>
 
-          <?php 
+          <?php
             if (count($content['field_founders_profile_student']['#items']) == 1) {
               print '<h3>Student Entrepreneur</h3>'; }
             else { print '<h3>The Team</h3>'; }
-            
-            print render($content['field_founders_profile_student']); 
+
+            print render($content['field_founders_profile_student']);
           ?>
         </div>
       </div>
       <div class="row">
         <div class="col-md-12">
-          
+
           <?php print render($content); ?>
 
           <?php if ($location): ?>
             <h3>Office</h3>
             <?php print render($content['field_founders_profile_location']); ?>
           <?php endif; ?>
-          
+
           <?php if ($contact): ?>
             <h3>Contact</h3>
             <?php print render($content['field_founders_profile_phone']); ?>
             <?php print render($content['field_founders_profile_email']); ?>
             <?php print render($content['field_founders_profile_url']); ?>
             <?php print render($content['field_founders_profile_facebook']); ?>
-            <?php print render($content['field_founders_profile_twitter']); ?> 
-          <?php endif; ?>         
-        </div>    
-      </div>   
+            <?php print render($content['field_founders_profile_twitter']); ?>
+            <?php print render($content['field_founders_profile_instagram']); ?>
+          <?php endif; ?>
+        </div>
+      </div>
 
     <?php
       // We hide the comments and links now so that we can render them later.
